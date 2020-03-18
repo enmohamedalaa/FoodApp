955b0c326ccdbecac7fd718120cc8268a741524a
enmohamedalaa <en_mohamed_alaa@yahoo.com>
2020-03-18 06:02:38 -0700

    New

---
+++ FoodApp/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  FoodApp
+//
+//  Created by Mohamed on 3/18/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
