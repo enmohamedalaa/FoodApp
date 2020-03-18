0efe15b5a6994f7dd21812829b73a0443a31248c
enmohamedalaa <en_mohamed_alaa@yahoo.com>
2020-03-18 06:00:22 -0700

    New

---
+++ FoodApp/CityHeaderCell.swift
@@ -0,0 +1,19 @@
+//
+//  CityHeaderCell.swift
+//  FoodApp
+//
+//  Created by Mohamed on 3/18/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class CityHeaderCell: UITableViewHeaderFooterView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Mohamed on 3/18/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tvMain: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tvMain.dataSource = self
+        tvMain.delegate = self
+        
+        tvMain.register(UINib(nibName: "CityHeaderCell", bundle: nil), forCellReuseIdentifier: "CityHeaderCell")
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate , UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tvMain.dequeueReusableHeaderFooterView(withIdentifier: "CityHeaderCell")
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+}
+
